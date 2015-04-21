2968ba3c0e0028378a935ad97c46f961f4cf533d
strati <strati@strati.hu>
2015-04-21 15:23:45 +0200

    update datadog with ec2 instance tag collection

--- manifests/datadog.pp
+++ manifests/datadog.pp
@@ -1,6 +1,8 @@
 if $datadog_enabled == 'true' {
   class { "datadog_agent":
-    api_key            => $datadog_api_key,
+    api_key                   => $datadog_api_key,
+    collect_ec2_tags          => true,
+    collect_instance_metadata => true
   }->
   class { 'datadog_agent::integrations::rabbitmq':
     url => 'http://localhost:15672/api/',
